b9fb73ebfd82d0b99bcc0020eda990a4c6b5fb7c
Mecca Jay <mecca.nicolej@gmail.com>
2020-02-03 01:39:56 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,12 +1,17 @@
 
+
+
+
+
 def hello_t(array)
  i = 0
   while i < array.length
-      yield array[i]
+      yield (array[i])
       i += 1
   end
   array
 end
 
 array = [1,2,3,4,5]
+
 hello_t(array) {|n| n > 3 }
