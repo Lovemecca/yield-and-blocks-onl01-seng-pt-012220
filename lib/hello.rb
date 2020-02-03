1d3beca0eccb9a243ef3ed99fbcccb341ac6f2fa
Mecca Jay <mecca.nicolej@gmail.com>
2020-02-03 02:30:34 +0000

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,20 @@
-def hello_t
 
+
+def hello_t(array)
+  if block_given?
+ i = 0
+  while i < array.length
+      yield(array[i])
+      i = i + 1
+  end
+  array
+  
+  else
+    puts "Hey! No block was given!"
+  end
 end
 
-# call your method here!
 
+array = [1,2,3,4,5]
+
+hello_t(array){|n| n > 1 }
